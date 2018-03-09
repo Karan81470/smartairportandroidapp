da115492915e1bc7482aaadaecbe8d81299840fd
Kirankumar <karan81470@gmail.com>
2018-03-09 08:46:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/evhackathon/smartairport/MainActivity.java
@@ -0,0 +1,367 @@
+package com.example.evhackathon.smartairport;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.snapshot.BeaconStateResult;
+import com.google.android.gms.awareness.state.BeaconState;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageFilter;
+import com.google.android.gms.nearby.messages.MessagesOptions;
+import com.google.android.gms.nearby.messages.NearbyMessagesStatusCodes;
+import com.google.android.gms.nearby.messages.NearbyPermissions;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
+import com.example.evhackathon.smartairport.Services.NearbyBackgroundService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks
+        , GoogleApiClient.OnConnectionFailedListener, ResultCallback<LocationSettingsResult> {
+
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    public static final int MY_BLUETOOTH_ENABLE_REQUEST_ID = 6;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_CODE = 1111;
+    private static final String KEY_SUBSCRIBED = "subscribed";
+    protected LocationRequest mLocationRequest;
+    protected LocationSettingsRequest mLocationSettingsRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mSubscribed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (savedInstanceState != null) {
+            mSubscribed = savedInstanceState.getBoolean(KEY_SUBSCRIBED, false);
+        }
+        if (!havePermissions()) {
+            Log.i(TAG, "Requesting permissions needed for this app.");
+            requestPermissions();
+        }
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, MY_BLUETOOTH_ENABLE_REQUEST_ID);
+    }
+
+    private View getRootView() {
+        return getWindow().getDecorView().getRootView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (havePermissions()) {
+            buildGoogleApiClient();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mGoogleApiClient != null) {
+            unSubscribe();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_SUBSCRIBED, mSubscribed);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        subscribe();
+        getBeaconInformationWithAwarenessAPI();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+
+    private synchronized void buildGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Awareness.API)
+                    .addApi(LocationServices.API)
+                    .addApi(Nearby.MESSAGES_API, new MessagesOptions.Builder()
+                            .setPermissions(NearbyPermissions.BLE).build())
+                    .addConnectionCallbacks(this)
+                    .enableAutoManage(this, this)
+                    .build();
+            mGoogleApiClient.connect();
+
+        }
+    }
+
+    protected void createLocationRequest() {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+    }
+
+    protected void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    protected void checkLocationSettings() {
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        mGoogleApiClient,
+                        mLocationSettingsRequest
+                );
+        result.setResultCallback(this);
+    }
+
+    private boolean havePermissions() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_CODE);
+    }
+
+    private void showLinkToSettingsSnackbar() {
+        if (getRootView() == null) {
+            return;
+        }
+        Snackbar.make(getRootView(),
+                R.string.permission_denied_explanation,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.settings, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Build intent that displays the App settings screen.
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package",
+                                BuildConfig.APPLICATION_ID, null);
+                        intent.setData(uri);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                }).show();
+    }
+
+    private void showRequestPermissionsSnackbar() {
+        if (getRootView() == null) {
+            return;
+        }
+        Snackbar.make(getRootView(), R.string.permission_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Request permission.
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                PERMISSIONS_REQUEST_CODE);
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != PERMISSIONS_REQUEST_CODE) {
+            return;
+        }
+        for (int i = 0; i < permissions.length; i++) {
+            String permission = permissions[i];
+            if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                if (shouldShowRequestPermissionRationale(permission)) {
+                    showRequestPermissionsSnackbar();
+                } else {
+                    showLinkToSettingsSnackbar();
+                }
+            } else {
+                buildGoogleApiClient();
+            }
+        }
+    }
+
+    private void publish(String message) {
+        Message mActiveMessage = new Message(message.getBytes());
+        Nearby.Messages.publish(mGoogleApiClient, mActiveMessage);
+    }
+
+    private PendingIntent getPendingIntent() {
+        return PendingIntent.getService(this, 0,
+                getBackgroundSubscribeServiceIntent(), PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private Intent getBackgroundSubscribeServiceIntent() {
+        return new Intent(this, NearbyBackgroundService.class);
+    }
+
+    private void subscribe() {
+        if (mSubscribed) {
+            return;
+        }
+        SubscribeOptions options = new SubscribeOptions.Builder()
+                .setStrategy(Strategy.BLE_ONLY)
+                .setFilter(new MessageFilter.Builder()
+                        .includeNamespacedType(getString(R.string.Namespace), getString(R.string.Type))
+                        .build()).build();
+
+        Nearby.Messages.subscribe(mGoogleApiClient, getPendingIntent(), options)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            Log.i(TAG, "Subscribed successfully.");
+                            startService(getBackgroundSubscribeServiceIntent());
+                        } else {
+                            Log.e(TAG, "Operation failed. Error: " +
+                                    NearbyMessagesStatusCodes.getStatusCodeString(
+                                            status.getStatusCode()));
+                        }
+                    }
+                });
+
+
+    }
+
+    private List<BeaconState.TypeFilter> setupAttachements() {
+        return Arrays.asList(
+                BeaconState.TypeFilter.with(
+                        getString(R.string.Namespace), getString(R.string.Type))
+        );
+    }
+
+    private void getBeaconInformationWithAwarenessAPI() {
+        if (!havePermissions()) {
+            Log.i(TAG, "Requesting permissions needed for this app.");
+            requestPermissions();
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions();
+        }
+        Awareness.SnapshotApi.getBeaconState(mGoogleApiClient, setupAttachements())
+                .setResultCallback(new ResultCallback<BeaconStateResult>() {
+                    @Override
+                    public void onResult(@NonNull BeaconStateResult beaconStateResult) {
+                        if (!beaconStateResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "Could not get beacon state.");
+                            return;
+                        }
+                        BeaconState beaconState = beaconStateResult.getBeaconState();
+                        if (beaconState != null) {
+                            List<BeaconState.BeaconInfo> beaconInfos = beaconState.getBeaconInfo();
+                            for (BeaconState.BeaconInfo beacon : beaconInfos) {
+                                Log.i(TAG, new String(beacon.getContent()));
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void unSubscribe() {
+        Nearby.Messages.unsubscribe(mGoogleApiClient, getPendingIntent());
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (getRootView() != null) {
+            Snackbar.make(getRootView(),
+                    "Exception while connecting to Google Play services: " +
+                            connectionResult.getErrorMessage(),
+                    Snackbar.LENGTH_INDEFINITE).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        getBeaconInformationWithAwarenessAPI();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                }
+                break;
+            case MY_BLUETOOTH_ENABLE_REQUEST_ID:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        createLocationRequest();
+                        buildLocationSettingsRequest();
+                        checkLocationSettings();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                //start location updates
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                try {
+                    status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                break;
+        }
+    }
+}
